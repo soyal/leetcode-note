cfe6368b886db770d306ddf7df5332102620c592
Soyal <345723940@qq.com>
2021-04-27 23:44:06 +0800

    path sum 3 the prefix sum algorithm need to be done

---
+++ simple/34.path-sum-3.js
@@ -0,0 +1,9 @@
+/**
+ * Given the root of a binary tree and an integer targetSum, return the number of paths where the sum of the values along the path equals targetSum.
+
+  The path does not need to start or end at the root or a leaf, but it must go downwards (i.e., traveling only from parent nodes to child nodes).
+
+  来源：力扣（LeetCode）
+  链接：https://leetcode-cn.com/problems/path-sum-iii
+  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
